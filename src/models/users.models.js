aaf1bfdd0c1b85a6bd68c928b3ca59473e1ae745
unknown <polibos6366@gmail.com>
2022-11-29 16:27:17 -0700

    project finished

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,43 @@
 
 
+const { Sequelize, DataTypes } = require('sequelize');
 
+const db = require('../utils/database');
 
+const Users = db.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATE,
+        allowNull: false
+
+    }
+
+}, {
+    timestamps: false // no agrega las columnas created_at y updated_at
+})
+
+module.exports = Users
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,2 +1,16 @@
 
 
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1234',
+    database: 'users'
+});
+
+
+
+module.exports = db
